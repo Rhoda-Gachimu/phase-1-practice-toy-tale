f92483684f6ae75859e6608fe239b6f5520e88c2
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-09-05 00:26:02 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,51 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+ 
+ fetch('http://localhost:3000/toys')  
+.then(response => response.json())
+.then(data => addToysToDom(data))
+
+
+const container = document.getElementById("toy-collection")
+function addToysToDom(data){
+  console.log(data)
+  data.forEach(toy => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "card")
+    const h2 = document.createElement("h2");
+    h2.textContent = `${toy.name}`
+    const img = document.createElement("img")
+    img.className = "toy-avatar"
+    img.src =`${toy.image}`
+    const p = document.createElement("p")
+    p.innerText = `${toy.likes}`
+    const button = document.createElement("button")
+    button.textContent = "Like ❤️"
+    button.id = `${toy.id}`
+    button.className="like-btn"
+//Append to the DOM
+    container.appendChild(div)
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(p)
+    div.appendChild(button)
+    button.addEventListener("click", () => {
+      p.innerText = incrementLikes(data)
+    })
+  })
+}
+function  addFromInput() {
+  const input = document.querySelector("form").elements[name="name"].value
+  const input2 = document.querySelector("form").elements[name="image"].value
+  console.log("input2", input)
+  return  {
+    "name" : input,
+    "image" : input2,
+    "likes" : 0,
+  }
+}
+  
+
